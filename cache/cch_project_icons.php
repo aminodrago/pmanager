ab8ec6d6d2e3d4d2349d248d0a5e9cfcc33d6bc1
Nopticon <nopticon@gmail.com>
2011-07-20 16:39:46 -0600

    const

--- cache/cch_project_icons.php
+++ cache/cch_project_icons.php
@@ -1,1 +1,1 @@
-<?php return unserialize('a:1:{i:2;a:2:{i:0;s:68:"http://10.10.10.148/pmanager/public/projects_icons/default.40x40.gif";i:1;s:68:"http://10.10.10.148/pmanager/public/projects_icons/default.16x16.gif";}}') ?>
+<?php return unserialize('a:2:{i:2;a:2:{i:0;s:68:"http://10.10.10.148/pmanager/public/projects_icons/default.40x40.gif";i:1;s:68:"http://10.10.10.148/pmanager/public/projects_icons/default.16x16.gif";}i:1;a:2:{i:0;s:61:"http://10.10.10.148/pmanager/projects_icons/default.40x40.gif";i:1;s:61:"http://10.10.10.148/pmanager/projects_icons/default.16x16.gif";}}') ?>
